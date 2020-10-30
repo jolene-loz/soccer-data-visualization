a4fc8f6946e7a84bde356cbfe3385be5cd0d1ef8
Jolene Lozano <lozanoja@bc.edu>
2020-10-30 14:45:29 -0400

    Revised data for goal location

--- goalLocation.js
+++ goalLocation.js
@@ -1,6 +1,30 @@
-let team1 = document.querySelector("#Team1").value;
-let team2 = document.querySelector("#Team2").value;
 
-d3.csv('games_goals_location.csv', d3.autoType).then(data => {
+
+    // width = 500 - margin.left - margin.right,
+    // height = 300 - margin.top - margin.bottom;
+
+d3.csv('game_goals_location_ontarget.csv', d3.autoType).then(data => {
+    //filter team1
+    // data = data.filter(data => data.team2 == team2)
+    //data2 = data.filter(data => data.event_team == team2)
     console.log(data)
-});
+
+    const svg = d3.select('.goalLocation')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    // let circles = svg.selectAll("circle")
+    //     .data(data.location)
+    //     .enter()
+    //     .append("circle")
+    //     .attr('cx', function(d, i){
+    //         return d.x;
+    //     })
+    //     .attr("cy", (d,i)=>d.y)
+    //     .attr("fill", "skyblue")
+    //     .attr('r', 4);
+
+
+    
+})
